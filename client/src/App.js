0546cb742bb149bb5358ab569d739c54450222ff
fullstackkg <khalilhgayle@gmail.com>
2024-11-19 14:14:31 -0500

    Add profile section

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,38 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import Home from './components/Home';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Home from "./components/Home";
+import Banner from "./components/Banner";
 
 function App() {
+  // Depending if user is authenticated or not, will determine what links to show
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   return (
     <Router>
+      <Banner isAuthenticated={isAuthenticated} />
       <Routes>
-        <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
+        <Route
+          path="/login"
+          element={<Login setIsAuthenticated={setIsAuthenticated} />}
+        />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={isAuthenticated ? <Home setIsAuthenticated={setIsAuthenticated} /> : <Navigate to="/login" />} />
+        <Route
+          path="/home"
+          element={
+            isAuthenticated ? (
+              <Home setIsAuthenticated={setIsAuthenticated} />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
         <Route path="/" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
---
+++ client/src/components/Profile.js
@@ -0,0 +1,2 @@
+// Update email
+// Update username
